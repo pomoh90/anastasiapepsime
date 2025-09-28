9e25362e340ea8062c68a5a336d140160767b602
pomoh90 <dacaball14121990@gmail.com>
2025-09-28 04:31:43 +0200

    Configure port 3035 in next.config.mjs and package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Настройка порта для production
+  env: {
+    PORT: '3035'
+  }
+};
 
 export default nextConfig;
